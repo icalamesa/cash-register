76be0a62d0dfec102dcda78e6f7c6ab02b73ace2
Ivan Cala Mesa <75147258+icalamesa@users.noreply.github.com>
2025-01-25 13:26:09 +0100

    Github Actions Workflow configuration with running examples (#2)
    * Valid and Empty Cart validations working
    
    * hotfix runner
    
    * workflow yaml configuration
    
    * modified requirements
    
    * fix pytest run command path
    
    ---------
    
    Co-authored-by: Ivan C. <contact@icalamesa.com>

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI, HTTPException
+from app.models import ProductInput
 
 app = FastAPI()
 
 @app.post("/cart/add")
-def add_to_cart(item: int, quantity: int):
-    try:
-        # add to cart logic
-        return {"message": f"Added {quantity} of {item} to the cart"}
-    except ValueError as e:
-        raise HTTPException(status_code=400, detail=str(e))
-    
+def add_to_cart(product: ProductInput):
+    """
+    Add a product to the cart.
+    Input: { "item": "GR1", "quantity": 1 }
+    Output: { "message": "Added 1 of GR1 to the cart" }
+    """
+    if not product.item.strip():
+        raise HTTPException(status_code=400, detail="Product item cannot be empty")
+    return {"message": f"Added {product.quantity} of {product.item} to the cart"}
