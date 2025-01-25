73a078908f28ac6772bae7d540ea9dbea29f734b
Ivan C. <contact@icalamesa.com>
2025-01-25 13:41:25 +0100

    working catalog implementation

--- app/catalog.py
+++ app/catalog.py
@@ -3,7 +3,11 @@ class Catalog:
     A simple catalog class to manage product details like codes, names, and prices.
     """
     def __init__(self):
-        None
+        self.products = {
+            "GR1": {"name": "Green Tea", "price": 3.11},
+            "SR1": {"name": "Strawberries", "price": 5.00},
+            "CF1": {"name": "Coffee", "price": 11.23},
+        }
 
     def get_product(self, code):
         """
