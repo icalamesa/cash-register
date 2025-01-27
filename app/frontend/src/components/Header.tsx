6901f67db12ccdd5828dd629917c49bf6676469b
Ivan C. <contact@icalamesa.com>
2025-01-27 20:37:49 +0100

    basic header and added dependency handling

---
+++ app/frontend/src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { FaShoppingCart } from "react-icons/fa";
+import "../styles.css";
+
+const Header: React.FC = () => {
+  return (
+    <header className="header">
+      <h1 className="header-title">Shopping Cart App</h1>
+      <button className="cart-button">
+        <FaShoppingCart />
+      </button>
+    </header>
+  );
+};
+
+export default Header;
